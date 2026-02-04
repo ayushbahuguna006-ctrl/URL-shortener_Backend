875dbeae335df32819bcd0ede8a3bcfe3ca7928a
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-02-04 12:16:53 +0530

    viws and home page rendered

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express=require ('express')
 const app=express()
+const path=require('path')
 const url=require('./Models/url')
 const router=require ('./Routes/url')
 const {connectTomongoDb}=require('./DB-Connection/url')
 connectTomongoDb('mongodb://127.0.0.1:27017/shorturl').then(()=>{console.log('DB connected')})
-app.use(express.json())  //middleware
+app.set('view engine','ejs')
+app.set('Views',path.resolve('../Views'))
+app.use(express.urlencoded({ extended: false }))   //helps in parsing human readable format 
+app.use(express.json())  //middleware //helps in sending json format parsing
 app.use('/url',router)
+app.get("/url/home",async(req,res)=>{
+     await res.render("home")
+})
 app.get("/:shortid",async(req,res)=>{
      try {
         const shortid = req.params.shortid;
@@ -26,6 +33,7 @@ app.get("/:shortid",async(req,res)=>{
         res.status(500).send("Server error");
     }
 })
+
 
 
 app.listen(8000,()=>{console.log("server started")})
